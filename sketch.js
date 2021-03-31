b0be80c30cf93c24a9687fac951dffc1a39ee719
mohd-faisal-code <74413710+mohd-faisal-code@users.noreply.github.com>
2021-03-31 11:35:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var balloon,balloonImage1,balloonImage2;
+var bg;
+// create database and position variable here
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  balloon=createSprite(100,400,20,20);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.4;
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    balloon.x = balloon.x - 10;
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    balloon.x = balloon.x + 10;
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    balloon.y = balloon.y - 10;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    balloon.y = balloon.y + 10;
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
